ae6cf590178ad26c7f8e43982e05b1f2a9b29dce
Peter Sevcik <PsevcikBN@gmail.com>
2024-04-07 10:54:13 +0200

    feat: update

--- Assets/Game/Scripts/TheFirstHit.cs
+++ Assets/Game/Scripts/TheFirstHit.cs
@@ -1,20 +1,7 @@
 using UnityEngine;
 
 public class TheFirstHit : MonoBehaviour {
-    [SerializeField] 
-    private AudioSource _sound;
-    
-    void Start() {
 
-        // var drunkSettings = Drunk.GetSettings();
-        // drunkSettings.drunkenness = 1f;
-        //
-        // var blurrySettings = Blurry.GetSettings();
-        // blurrySettings.strength = 1f;
-        //
-        // Guide.Instance.ShowGuide("HELLO", 3f, 3f);
-    }
-    
     private bool _firstHitDone;
     private float _currentDuration = 0f;
     private float _duration = 4f;
@@ -23,20 +10,15 @@ public class TheFirstHit : MonoBehaviour {
         
         _currentDuration += Time.deltaTime;
         var normalizedDuration = _currentDuration / _duration;
-        var value = Mathf.Lerp(10f, 0.4f, normalizedDuration);
-        
-        // var drunkSettings = Drunk.GetSettings();
-        // drunkSettings.drunkenness = value;
-        //
-        // var blurrySettings = Blurry.GetSettings();
-        // blurrySettings.strength = value - 0.3f;
+        var value = Mathf.Lerp(1f, 0.3f, normalizedDuration);
+
+        PostProcessing.Instance.ChromaticAberration(value);
+        PostProcessing.Instance.LensDistortion(value);
         
         if (normalizedDuration >= 1f) {
             _firstHitDone = true;
-            
-            // drunkSettings.drunkenness = 0.4f;
-            // blurrySettings.strength = 0.1f;
+            PostProcessing.Instance.ChromaticAberration(0.3f);
+            PostProcessing.Instance.LensDistortion(0f);
         }
-        
     }
 }
---
+++ Assets/PostProcessing.cs
@@ -0,0 +1,45 @@
+
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class PostProcessing : MonoBehaviour {
+    
+    // ======================== SINGLETON ========================
+    private void Awake() {
+        if (_instance == null) {
+            _instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    private static PostProcessing _instance;
+    public static PostProcessing Instance => _instance;
+    // ======================== ======== ========================
+
+    private void Start() {
+        if (_volume.profile.TryGet<ChromaticAberration>(out var chromaticAberration)) {
+            _chromaticAberration = chromaticAberration;
+        }
+        
+        if (_volume.profile.TryGet<LensDistortion>(out var lensDistortion)) {
+            _lensDistortion = lensDistortion;
+        }
+    }
+
+    [SerializeField]
+    private Volume _volume;
+
+    private ChromaticAberration _chromaticAberration;
+    private LensDistortion _lensDistortion;
+    
+    public void ChromaticAberration(float value) {
+        _chromaticAberration.intensity.value = value;
+    }
+    
+    public void LensDistortion(float value) {
+        _lensDistortion.intensity.value = value;
+    }
+}
